f878283ff261af04b6db801dcba63478ed804bd5
nayudesu <srkm.149@gmail.com>
2022-07-09 09:13:05 +0000

    管理者　コメント削除機能（リダイレクト先のパス指定）

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,12 +1,10 @@
 class Admin::PostCommentsController < ApplicationController
-  
+
   before_action :authenticate_admin!
-  
+
   def destroy
-    @post = Post.find(params[:id])
-    @post_comment = PostComment.find(params[:id])
-    @post_comment.destroy
-    redirect_to admin_post_path(@post)
-  end 
-  
+    PostComment.find(params[:id]).destroy
+    redirect_to admin_post_path(params[:post_id])
+  end
+
 end
