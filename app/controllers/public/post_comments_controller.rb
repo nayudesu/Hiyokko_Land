3232df62d6bc8ee565cf755c86f3a227a65ff433
nayudesu <srkm.149@gmail.com>
2022-07-12 01:44:58 +0000

    コメントcreate 非同期

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -3,13 +3,14 @@ class Public::PostCommentsController < ApplicationController
   before_action :authenticate_member!
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
     comment = current_member.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     PostComment.find(params[:id]).destroy
   end
 
