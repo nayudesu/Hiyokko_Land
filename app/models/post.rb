4b2aedcd5b4271a6b4e3e99dab61c55e8e2af72c
nayudesu <srkm.149@gmail.com>
2022-07-13 09:12:02 +0000

    バリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,9 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
+  validates :title, presence: true, length:{ maximum: 70 }
+  validates :body, presence: true, length:{ maximum: 800 }
+
   def get_image(width, height)
     unless image.attached?
     file_path = Rails.root.join('app/assets/images/no-image.jpeg')
