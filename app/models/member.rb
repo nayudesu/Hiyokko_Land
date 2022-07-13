040f6892d47119c167970cc560751cd08a79d090
nayudesu <srkm.149@gmail.com>
2022-07-13 04:29:34 +0000

    画像のコード変更

--- app/models/member.rb
+++ app/models/member.rb
@@ -10,6 +10,14 @@ class Member < ApplicationRecord
 
   has_one_attached :profile_image
 
+  def get_profile_image(width, height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image-icon.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,14 @@ class Post < ApplicationRecord
 
   has_one_attached :image
 
+  def get_image(width, height)
+    unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no-image.jpeg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def favorited_by?(member)
     favorites.exists?(member_id: member.id)
   end
