7c366e583dc355436b92b9e0e2be4150c29ca2fe
nayudesu <srkm.149@gmail.com>
2022-07-15 03:11:11 +0000

    トップ画像設定

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,9 +1,9 @@
 class Public::HomesController < ApplicationController
-  
+
   def top
-    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 
   def about
+    @all_ranks = Post.find(Favorite.group(:post_id).order('count(post_id) desc').limit(3).pluck(:post_id))
   end
 end
